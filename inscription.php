c7ef20a8b4f7bf389f19b9f6ae6ec9d2ced1b266
tshiivaa <131045338+tshiivaa@users.noreply.github.com>
2024-03-25 22:18:08 +0100

    sepration MODEL

--- inscription.php
+++ inscription.php
@@ -1,55 +1,189 @@
- 
-  <?php  include 'connexion.php'; 
-    $nameErr = ""; $validForm=true;
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["nom"])) {
-    $nameErr = "Name is required";
-    $validForm= false;
-  } else {
-    $nameErr="";
-    //$name = test_input($_POST["name"]);
-  }
- 
+<?php global $conn;
+include 'connexion.php';
+include 'utilisateurs.php';
+$emailErr = "";
+$dateErr = "";
+$passwordErr = "";
+$validForm = true;
+
+
+if (array_key_exists('save', $_POST)) {
+    testAndSave();
+
 }
-    if(isset($_POST['nom'])&& $validForm)
+if (array_key_exists('connect', $_POST)) {
+    connect();
+
+}
+function testAndSave()
 {
-    $sql = "INSERT INTO utilisateurs (nom, prénom, date_nais) VALUES (:nom, :prenom, :date_nais)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute( [':nom' => $_POST["nom"],':prenom' => $_POST["prenom"],':date_nais' => $_POST["date_nais"] ]   );
-    $stmt = null;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["email"])) {
+            $GLOBALS['emailErr'] = "Email is required";
+            $GLOBALS['validForm'] = false;
+        } else {
+            $email = htmlspecialchars($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $GLOBALS['emailErr'] = "Invalid email format";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (empty($_POST["date_nais"])) {
+            $GLOBALS['dateErr'] = "date is required";
+
+            $GLOBALS['validForm'] = false;
+        } else {
+            $date = htmlspecialchars($_POST["date_nais"]);
+            // You may need additional validation for the date of birth
+            if (!strtotime($date)) {
+                $GLOBALS['dateErr'] = "Invalid date format";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (empty($_POST["password"])) {
+            $GLOBALS['passwordErr'] = "password is required";
+            $GLOBALS['validForm'] = false;
+        } else {
+            $password = htmlspecialchars($_POST["password"]);
+            if (strlen($password) < 8) {
+                $GLOBALS['passwordErr'] = "Invalid 8 characters min";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (isset($_POST['email']) && $GLOBALS['validForm']) {
+            $sql = "INSERT INTO utilisateurs (email,date_nais,password) VALUES (:email, :date_nais, :password)";
+            $stmt = $GLOBALS['conn']->prepare($sql);
+            $utilisateur = new utilisateurs($_POST["email"], $_POST["date_nais"], $_POST["password"]);
+            $stmt->execute([':email' => $utilisateur->getEmail(), ':date_nais' => $utilisateur -> getdate_nais(), ':password' => $utilisateur -> getpassword()]);
+            $stmt = null;
+        }
+
+    }
 }
-     ?>
- 
- <!DOCTYPE html>
+
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FORMULAIRE</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script
+            src="https://kit.fontawesome.com/64d58efce2.js"
+            crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="inscription.css"/>
+    <title>Formulaire de connexion & d'inscription</title>
 </head>
 <body>
-    <form action="" method="POST">
+<div class="container">
+    <div class="forms-container">
+        <div class="signin-signup">
+            <form action="#" class="sign-in-form">
+                <h2 class="title">Se connecter</h2>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" placeholder="Adresse e-mail"/>
+                </div>
+                <div class="input-field">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Mot de passe"/>
+                </div>
+                <input type="submit" name="connect" value="Connexion" class="btn solid"/>
+                <p class="social-text">Ou connectez-vous avec les plateformes sociales</p>
+                <div class="social-media">
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-instagram"></i>
+                    </a>
 
-        <label for="nom">Nom:</label>
-        <input type="text" id="nom" name="nom"> <br>
-        <?php if(!empty($nameErr)): ?>
-<span class="error"> *<?php   echo $nameErr; ?> </span> <br> <?php endif; ?>
-        <label for="prenom">Prénom:</label>
-        <input type="text" id="prenom" name="prenom"><br>
+                </div>
+            </form>
+            <form action="" method="POST" class="sign-up-form">
+                <h2 class="title">S'inscrire</h2>
+                <div class="input-field">
+                    <i class="fas fa-envelope"></i>
+                    <input type="text" placeholder="Adresse e-mail" name="email"/>
 
-        <label for="date_nais">date_nais:</label>
-        <input type="date" id="date_nais" name="date_nais"><br>
 
-        <input type="submit" value="Envoyer">
-        <input type="reset" value="Annuler">
+                </div>
+                <div class="error"><?php if (!empty($emailErr)) echo $emailErr; ?></div>
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="date" placeholder="date nais" name="date_nais"/>
 
-    </form>
-  
-   
 
-    
-    
+                </div>
+                <div class="error"><?php if (!empty($dateErr)) echo $dateErr; ?></div>
+                <div class="input-field">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Mot de passe" name="password"/>
+
+                </div>
+                <div class="error"><?php if (!empty($passwordErr)) echo $passwordErr; ?></div>
+                <input name="save" type="submit" class="btn" value="S'inscrire"/>
+
+                <p class="social-text">Ou inscrivez-vous avec les plateformes sociales</p>
+                <div class="social-media">
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="panels-container">
+        <div class="panel left-panel">
+            <div class="content">
+                <h3>VOYSYNC</h3>
+                <p>
+                    Bienvenue sur la page du site web de VOYSYNC. Complétez votre inscription pour accéder au
+                    site web.
+                </p>
+                <button class="btn transparent" id="sign-up-btn">
+                    S'inscrire
+                </button>
+            </div>
+            <img src="yello1.png" class="image" alt=""/>
+        </div>
+        <div class="panel right-panel">
+            <div class="content">
+                <h3>VOYSYNC</h3>
+                <p>
+                    Bienvenue sur la page du site web de VOYSYNC. Connectez-vous pour accéder au site web.
+                </p>
+                <button class="btn transparent" id="sign-in-btn">
+                    Se connecter
+                </button>
+            </div>
+            <img src="yello1.png" class="image" alt=""/>
+        </div>
+    </div>
+</div>
+
+<script>
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+
+    sign_up_btn.addEventListener("click", () => {
+        container.classList.add("sign-up-mode");
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+        container.classList.remove("sign-up-mode");
+    });
+</script>
 </body>
 </html>
+
 
 
---
+++ utilisateurs.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+class utilisateurs
+{
+    private string $email;
+    private string $date_nais;
+    private string $password;
+
+    public function __construct($email, $date_nais, $password)
+    {
+        $this->email = $email;
+        $this->date_nais = $date_nais;
+        $this->password = $password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getdate_nais()
+    {
+        return $this->date_nais;
+    }
+
+    public function getpassword()
+    {
+        return $this->password;
+    }
+
+}
+
+//instance
+
+
+?>
+</body>
+</html>
