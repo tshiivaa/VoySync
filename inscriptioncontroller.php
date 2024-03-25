601c1816e0c61a11f82eedf372723e25453ed7af
tshiivaa <131045338+tshiivaa@users.noreply.github.com>
2024-03-25 22:43:58 +0100

    MVC

---
+++ inscriptioncontroller.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include 'connexion.php';
+include 'utilisateurs.php';
+ $emailErr = "";
+ $dateErr = "";
+ $passwordErr = "";
+ $validForm = true;
+
+ Class utilisateurc{
+
+function testAndSave()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["email"])) {
+            $GLOBALS['emailErr'] = "Email is required";
+            $GLOBALS['validForm'] = false;
+        } else {
+            $email = htmlspecialchars($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $GLOBALS['emailErr'] = "Invalid email format";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (empty($_POST["date_nais"])) {
+            $GLOBALS['dateErr'] = "date is required";
+
+            $GLOBALS['validForm'] = false;
+        } else {
+            $date = htmlspecialchars($_POST["date_nais"]);
+            // You may need additional validation for the date of birth
+            if (!strtotime($date)) {
+                $GLOBALS['dateErr'] = "Invalid date format";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (empty($_POST["password"])) {
+            $GLOBALS['passwordErr'] = "password is required";
+            $GLOBALS['validForm'] = false;
+        } else {
+            $password = htmlspecialchars($_POST["password"]);
+            if (strlen($password) < 8) {
+                $GLOBALS['passwordErr'] = "Invalid 8 characters min";
+                $GLOBALS['validForm'] = false;
+            }
+        }
+
+        if (isset($_POST['email']) && $GLOBALS['validForm']) {
+            $sql = "INSERT INTO utilisateurs (email,date_nais,password) VALUES (:email, :date_nais, :password)";
+            $stmt = $GLOBALS['conn']->prepare($sql);
+            $utilisateur = new utilisateurs($_POST["email"], $_POST["date_nais"], $_POST["password"]);
+            $stmt->execute([':email' => $utilisateur->getEmail(), ':date_nais' => $utilisateur->getdate_nais(), ':password' => $utilisateur->getpassword()]);
+            $stmt = null;
+        }
+
+    }
+}
+}
+
+?>
+</tbody>
+</table>
+
+</body>
+</html>
--- inscriptionview.php
+++ inscriptionview.php
@@ -1,69 +1,17 @@
-<?php global $conn;
-include 'connexion.php';
-include 'utilisateurs.php';
-$emailErr = "";
-$dateErr = "";
-$passwordErr = "";
-$validForm = true;
+<?php
+include 'inscriptioncontroller.php';
 
 
 if (array_key_exists('save', $_POST)) {
-    testAndSave();
+    $x = new utilisateurc() ;
+    $x->testAndSave();
 
 }
 if (array_key_exists('connect', $_POST)) {
-    connect();
+    $x = new utilisateurc() ;
+    $x->testAndSave();
 
 }
-function testAndSave()
-{
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["email"])) {
-            $GLOBALS['emailErr'] = "Email is required";
-            $GLOBALS['validForm'] = false;
-        } else {
-            $email = htmlspecialchars($_POST["email"]);
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $GLOBALS['emailErr'] = "Invalid email format";
-                $GLOBALS['validForm'] = false;
-            }
-        }
-
-        if (empty($_POST["date_nais"])) {
-            $GLOBALS['dateErr'] = "date is required";
-
-            $GLOBALS['validForm'] = false;
-        } else {
-            $date = htmlspecialchars($_POST["date_nais"]);
-            // You may need additional validation for the date of birth
-            if (!strtotime($date)) {
-                $GLOBALS['dateErr'] = "Invalid date format";
-                $GLOBALS['validForm'] = false;
-            }
-        }
-
-        if (empty($_POST["password"])) {
-            $GLOBALS['passwordErr'] = "password is required";
-            $GLOBALS['validForm'] = false;
-        } else {
-            $password = htmlspecialchars($_POST["password"]);
-            if (strlen($password) < 8) {
-                $GLOBALS['passwordErr'] = "Invalid 8 characters min";
-                $GLOBALS['validForm'] = false;
-            }
-        }
-
-        if (isset($_POST['email']) && $GLOBALS['validForm']) {
-            $sql = "INSERT INTO utilisateurs (email,date_nais,password) VALUES (:email, :date_nais, :password)";
-            $stmt = $GLOBALS['conn']->prepare($sql);
-            $utilisateur = new utilisateurs($_POST["email"], $_POST["date_nais"], $_POST["password"]);
-            $stmt->execute([':email' => $utilisateur->getEmail(), ':date_nais' => $utilisateur -> getdate_nais(), ':password' => $utilisateur -> getpassword()]);
-            $stmt = null;
-        }
-
-    }
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
